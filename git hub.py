19764056d979e35f2e4efb845adff2049d7a40fa
Jasbleidy Pilonieta <andreapilonieta35@gmail.com>
2021-09-07 15:04:11 -0500

    mensaje

---
+++ git hub.py
@@ -0,0 +1,37 @@
+"""
+jasbleidy Andreina Pilonieta
+07/09}72021
+"""
+def saludar ():
+    print ("Hola, bienvenid@")
+    print ("¿Como te llamas?")
+    saludar = input ()
+    
+saludar()
+
+print ("¿Como te encuentras?")
+emocion=input ()
+
+if emocion == "bien":
+    print ("Me alegra :3")
+
+elif emocion == "mal":
+    print ("Oo que mal, espero todo mejore")
+       
+print ("¿trabajas o estudias?")
+ocupacion=input()
+
+if ocupacion == "trabajo":
+    print ("¿En que trabajas?")
+    trabajo=input()
+    print ("Exelente que bueno")
+
+if ocupacion == "estudio":
+    print ("¿En que estudias universidad o colegio?")
+    estudio=input()
+    print ("espero que te valla bien en tus estudios")
+    
+
+    
+
+
